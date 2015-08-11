4041bfb7879402f6089e214b41547e1bc012df85
U-ARRS\bxiong <bxiong@pc-bxiong.ARRS.ARRISI.com>
2015-08-11 17:28:52 +0800

    Add serial port control

--- WebControl/process.c
+++ WebControl/process.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <termios.h>
 
 #include "logger.h"
 #include "process.h"
@@ -25,6 +26,8 @@ int     connect_server(void);
 int     create_process(const char* file, const char* argv);
 void*   keeponline(void* arg);
 void    send_msg(char* msg);
+int     opencom(char* dev);
+int     set_opt(int fd, int nSpeed, int nBits, char nEvent, int nStop);
 
 
 void send_msg(char* msg)
@@ -151,3 +154,120 @@ int   create_process(const char* file, const char* argv)
 }
 
 
+int opencom(char* dev)
+{
+    int fd = 0;
+
+    fd = open(dev, O_RDWR | O_NOCTTY | O_NDELAY);
+    if(fd == -1)
+    {
+        debug("Can't open serial port %s\n", dev);
+        return fd;
+    }
+
+    if(fcntl(fd, F_SETFL, 0) < 0)
+    {
+        debug("fcntl failed! port: %s\n", dev);
+    }
+    else
+    {
+        debug("fcntl = %d, port: %s\n", fcntl(fd, F_SETFL, 0), dev);
+    }
+
+    if(isatty(STDIN_FILENO) == 0)
+    {
+        debug("standard input is not a terminal device\n");
+    }
+    else
+    {
+        debug("isatty success!\n");
+    }
+
+    debug("open serial port %s successfully, fd: %d\n", dev, fd);
+
+    return fd;
+}
+
+
+int set_opt(int fd, int nSpeed, int nBits, char nEvent, int nStop)
+{
+    struct termios newtio,oldtio;
+
+    if  ( tcgetattr( fd,&oldtio)  !=  0) 
+    { 
+        perror("SetupSerial 1");
+        return -1;
+    }
+    bzero( &newtio, sizeof( newtio ) );
+    newtio.c_cflag  |=  CLOCAL | CREAD; 
+    newtio.c_cflag &= ~CSIZE; 
+
+    switch( nBits )
+    {
+    case 7:
+        newtio.c_cflag |= CS7;
+        break;
+    case 8:
+        newtio.c_cflag |= CS8;
+        break;
+    }
+
+    switch( nEvent )
+    {
+    case 'O':                     //奇校验
+        newtio.c_cflag |= PARENB;
+        newtio.c_cflag |= PARODD;
+        newtio.c_iflag |= (INPCK | ISTRIP);
+        break;
+    case 'E':                     //偶校验
+        newtio.c_iflag |= (INPCK | ISTRIP);
+        newtio.c_cflag |= PARENB;
+        newtio.c_cflag &= ~PARODD;
+        break;
+    case 'N':                    //无校验
+        newtio.c_cflag &= ~PARENB;
+        break;
+    }
+
+    switch( nSpeed )
+    {
+    case 2400:
+        cfsetispeed(&newtio, B2400);
+        cfsetospeed(&newtio, B2400);
+        break;
+    case 4800:
+        cfsetispeed(&newtio, B4800);
+        cfsetospeed(&newtio, B4800);
+        break;
+    case 9600:
+        cfsetispeed(&newtio, B9600);
+        cfsetospeed(&newtio, B9600);
+        break;
+    case 115200:
+        cfsetispeed(&newtio, B115200);
+        cfsetospeed(&newtio, B115200);
+        break;
+    default:
+        cfsetispeed(&newtio, B9600);
+        cfsetospeed(&newtio, B9600);
+        break;
+    }
+    if( nStop == 1 )
+    {
+        newtio.c_cflag &=  ~CSTOPB;
+    }
+    else if ( nStop == 2 )
+    {
+        newtio.c_cflag |=  CSTOPB;
+    }
+    newtio.c_cc[VTIME]  = 0;
+    newtio.c_cc[VMIN] = 0;
+    tcflush(fd,TCIFLUSH);
+    if((tcsetattr(fd,TCSANOW,&newtio))!=0)
+    {
+        perror("com set error");
+        return -1;
+    }
+    printf("set done!\n");
+    return 0;
+}
